3b1eccac7b91e657ed823c978c1fc633212d8002
Shayan Rais <shayan.rais@innovativesolutions.ae>
2016-04-08 12:06:25 +0500

    comments added

--- app/src/main/java/com/gittest/MainActivity.java
+++ app/src/main/java/com/gittest/MainActivity.java
@@ -13,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     //2 - Download New Project from Github into Another PC
     //https://www.londonappdeveloper.com/how-to-clone-a-github-project-on-android-studio/
     //pdf is downloaded and present at Project/Shayan/
+    //Since it is not working therefore try this
+    /*Tutorial : http://stackoverflow.com/questions/31752042/how-can-i-import-android-studio-project-from-github
+        1 - Create Folder "GitClone"
+        2 - Right Click on Folder, Select Git Bash Here
+        3 - Write git remote add origin https://github.com/isolpak1/GitTest
+        4 - Create new file settings.gradle
+        5 - Edit file and add include ':app'
+        6 - Open Android Studio, go to File, Import Project
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
