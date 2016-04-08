a83d86a9750eb8b7fc55dd65f3df4f29f01069b7
Shayan Rais <shayan.rais@innovativesolutions.ae>
2016-04-08 11:45:57 +0500

    initial commit

---
+++ app/src/main/java/com/gittest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //1 - Create a Project and upload it to Github
+    //Complete Tutorial is followed from here,
+    //https://www.londonappdeveloper.com/how-to-use-git-hub-with-android-studio/
+    //pdf is downloaded and present at Project/Shayan/
+
+    //2 - Download New Project from Github into Another PC
+    //https://www.londonappdeveloper.com/how-to-clone-a-github-project-on-android-studio/
+    //pdf is downloaded and present at Project/Shayan/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
